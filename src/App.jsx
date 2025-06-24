8c44f4f776b72b9f55f5e66047a23876ff9959a6
cnorman2000 <cnorman2000@gmail.com>
2025-06-24 09:58:51 +0100

    setting up Vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <Routes></Routes>
+    </>
+  );
+}
+
+export default App;
